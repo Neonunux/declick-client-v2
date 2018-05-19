0548f6ea35a42b828fd0a5ecb0e51ed144551ba9
Yovar <yovar@rougain.com>
2018-05-19 13:07:28 +0000

    webpack files for hotreload

---
+++ webpack.config.development.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: [
+      path.resolve(__dirname, '..', 'dist'),
+    ],
+    watchContentBase: true,
+    hot: true,
+    public: 'fcc44c0356ec48c391df95af4f538acf.vfs.cloud9.eu-west-1.amazonaws.com',
+    allowedHosts: [
+      'fcc44c0356ec48c391df95af4f538acf.vfs.cloud9.eu-west-1.amazonaws.com',
+    ],
+  },
+}

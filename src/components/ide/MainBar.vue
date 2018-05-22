30bd86ed315b54cfd656646848bc0573b48a4b31
Yovar <yovar@rougain.com>
2018-05-22 22:25:26 +0000

    prettify MainBar

--- src/components/ide/MainBar.vue
+++ src/components/ide/MainBar.vue
@@ -13,20 +13,76 @@
 @import '~@/assets/styles/mixins'
 
 .main-bar
-  height: 50px
+  display: flex
+  align-items: center
+  justify-items: center
   background: #ddd6dd
 
+.main-bar > *
+  height: 36px
+  width: 36px
+  margin: 9px
+  border: 0
+  cursor: pointer
+  outline: 0
+
 .main-bar
-  & > button
-    height: 36px
-    width: 36px
-    border: 0
+  & > * + *
+    margin-left: 0
+
+/*
+             .-----.
+            /       `\
+          _|_         |
+         /   \        |
+         '==='        |
+         . ' .        |
+        . : ' .       |
+           '.         |
+       . '    .       |
+        .-"""-.       |
+       /  \___ \      |
+       |/`    \|      |
+       (  a  a )      |
+       |   _\ |       |
+       )\  =  /       |
+   _.-'  '---;        |
+ /`           `-.     |
+|                \    |
+|    |   .  & .   \   |
+\    /      &   |  ;  |
+|   |           |  ;  |
+|   /\          /  |  |
+\   \ )   -:-  /\  \  |
+ `.  `-.  -:-  | \  \_|
+   '-.  `-.    (  './\`\
+    / `'-. `\  |    \/_/
+    |    \  |  |      |
+    |    /'-\  /      |
+     \   \   | |      |
+      \   )_/\ |      |
+       \      \|      |
+        \      \      |
+         '.     |     |
+           /   /      |
+          /  .';      |
+        /`  /  |      |
+       /   /   |      |
+ jgs  |  .' \  |      |
+      /  \  )  |      |
+      \   \ /  '-.._  |
+       '.ooO\__._.Ooo |
+*/
+
+.main-bar
   &__wiki
-    +image-button('~@/assets/images/play.png')
+    +image-button('~@/assets/images/wiki.png')
   &__hints
     +image-button('~@/assets/images/help.png')
+    margin-right: auto
   &__play
     +image-button('~@/assets/images/play.png')
   &__save
     +image-button('~@/assets/images/save.png')
+    margin-left: auto
 </style>

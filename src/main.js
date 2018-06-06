405cbff5d49932f3f65fbdc266be9955219d7619
TheFloow <neonunux@gmail.com>
2018-06-06 17:55:59 +0200

    remove old vue vue

--- src/main.js
+++ src/main.js
@@ -1,45 +1,10 @@
 import $ from 'jquery'
 
-<<<<<<< HEAD
 import TFrame from '@/components/TFrame'
 import TUI from '@/components/TUI'
 import TProject from '@/data/TProject'
 import TEnvironment from '@/env/TEnvironment'
 import TRuntime from '@/run/TRuntime'
-=======
-// import TProject from '@/data/TProject'
-// import TEnvironment from '@/env/TEnvironment'
-// import TRuntime from '@/run/TRuntime'
-// import TFrame from '@/components/TFrame'
-// import TUI from '@/components/TUI'
-
-// require('@/assets/styles/style.css')
-// require('@/assets/styles/hints.css')
-// require('@/assets/styles/prism.css')
-// require('@/assets/styles/split-pane.css')
-// require('@/assets/styles/wColorPicker.min.css')
-// require('@/assets/styles/wPaint.min.css')
-// require('intro.js/themes/introjs-dark.css')
-// require('intro.js/themes/introjs-flattener.css')
-// require('intro.js/themes/introjs-modern.css')
-// require('intro.js/themes/introjs-nassim.css')
-// require('intro.js/themes/introjs-nazanin.css')
-// require('intro.js/themes/introjs-royal.css')
-// require('intro.js/introjs.css')
-
-import Vue from 'vue'
-
-import Ide from '@/components/ide/Ide.vue'
-import i18n from './i18n'
-
-const targetNode = document.createElement('div')
-targetNode.setAttribute('id', 'application')
-document.body.appendChild(targetNode)
-new Vue({
-  render: h => h(Ide),
-  i18n,
-}).$mount('#application')
->>>>>>> d37d87c13b87cbf53d15355c453d166f827a343d
 
 // Start the main app logic.
 function load() {
